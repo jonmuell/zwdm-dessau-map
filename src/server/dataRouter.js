d9a7555fc3532d2c00af27c28c6f54849ee6cac9
Jonas Müller <mueller.jonas95@gmail.com>
2020-10-07 18:37:15 +0200

    Add event list to sidebar

--- src/server/dataRouter.js
+++ src/server/dataRouter.js
@@ -5,9 +5,22 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 const data = require("./data.json");
+const events = require("./events.json");
 
 router.get('/', (req, res) => {
   res.send(data);
+});
+
+router.get('/events', (req, res) => {
+  const pageName = req.query.pageName
+
+  const filteredEvents = events[pageName];
+
+  if(filteredEvents) {
+    res.send(filteredEvents);
+  } else {
+    res.send([]);
+  }
 });
 
 router.get('/add', (req, res) => {
